773c89f4e14e0f86f01ef0f7ecb98c5aa802ad91
anissa0511 <dingziling@shunwang.com>
2019-07-22 16:28:06 +0800

    router

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import vTitle from "./title.vue";
+import vButton from "./button.vue";
+export default {
+  
+};
+</script>
+<style scoped>
+div {
+  color: #f60;
+  font-size: 24px;
+}
+</style>
